7121a43699cd4b77babb90ce242c93a4e8fb1bdf
Sayam Agarwal <97225527+Sayam241020@users.noreply.github.com>
2022-10-08 23:28:56 +0530

    Add files via upload

---
+++ kadane_algo.cpp
@@ -0,0 +1,37 @@
+<h1 align="center">Hi 👋, I'm Sayam Agarwal</h1>
+<h3 align="center">Competitive Programmer and Open Source Contributer</h3>
+<img align="right" alt="coding" width="400" src="https://miro.medium.com/max/1400/1*UFK4FjRiapOJ44dwksBanw.gif">
+<p align="left"> <img src="https://komarev.com/ghpvc/?username=sayam241020&label=Profile%20views&color=0e75b6&style=flat" alt="sayam241020" /> </p>
+
+<p align="left"> <a href="https://github.com/ryo-ma/github-profile-trophy"><img src="https://github-profile-trophy.vercel.app/?username=sayam241020" alt="sayam241020" /></a> </p>
+
+- 🌱 I’m currently learning **Flask**
+
+- 💬 Ask me about **Python, C++, Django, Html, CSS, Javascript**
+
+- 📫 How to reach me **iitsayamagarwal24@gmail.com**
+
+- 📄 Know about my experiences [https://drive.google.com/file/d/1wj1iaqpMVFAlXYW2M4YPDytAz6jcB2PP/view?usp=sharing](https://drive.google.com/file/d/1wj1iaqpMVFAlXYW2M4YPDytAz6jcB2PP/view?usp=sharing)
+
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+<a href="https://twitter.com/statist0714932" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg" alt="statist0714932" height="30" width="40" /></a>
+<a href="https://linkedin.com/in/https://www.linkedin.com/in/sayam-agarwal-001305216" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="https://www.linkedin.com/in/sayam-agarwal-001305216" height="30" width="40" /></a>
+<a href="https://fb.com/https://www.facebook.com/divyam.agarwal.71868/" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/facebook.svg" alt="https://www.facebook.com/divyam.agarwal.71868/" height="30" width="40" /></a>
+<a href="https://instagram.com/https://www.instagram.com/sayam.agarwal.241020/" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="https://www.instagram.com/sayam.agarwal.241020/" height="30" width="40" /></a>
+<a href="https://www.codechef.com/users/https://www.codechef.com/users/sayam241020" target="blank"><img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.1.0/icons/codechef.svg" alt="https://www.codechef.com/users/sayam241020" height="30" width="40" /></a>
+<a href="https://www.hackerrank.com/https://www.hackerrank.com/iitsayamagarwal1" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/hackerrank.svg" alt="https://www.hackerrank.com/iitsayamagarwal1" height="30" width="40" /></a>
+<a href="https://codeforces.com/profile/https://codeforces.com/profile/sayam241020" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/codeforces.svg" alt="https://codeforces.com/profile/sayam241020" height="30" width="40" /></a>
+<a href="https://www.leetcode.com/https://leetcode.com/sayam241020/" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/leet-code.svg" alt="https://leetcode.com/sayam241020/" height="30" width="40" /></a>
+<a href="https://www.hackerearth.com/https://www.hackerearth.com/@iitsayamagarwal24" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/hackerearth.svg" alt="https://www.hackerearth.com/@iitsayamagarwal24" height="30" width="40" /></a>
+<a href="https://auth.geeksforgeeks.org/user/https://auth.geeksforgeeks.org/user/iitsayamagarwal24/" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/geeks-for-geeks.svg" alt="https://auth.geeksforgeeks.org/user/iitsayamagarwal24/" height="30" width="40" /></a>
+</p>
+
+<h3 align="left">Languages and Tools:</h3>
+<p align="left"> <a href="https://getbootstrap.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" width="40" height="40"/> </a> <a href="https://www.cprogramming.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg" alt="c" width="40" height="40"/> </a> <a href="https://www.w3schools.com/cpp/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="cplusplus" width="40" height="40"/> </a> <a href="https://www.djangoproject.com/" target="_blank" rel="noreferrer"> <img src="https://cdn.worldvectorlogo.com/logos/django.svg" alt="django" width="40" height="40"/> </a> <a href="https://heroku.com" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg" alt="heroku" width="40" height="40"/> </a> <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> </a> <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a> <a href="https://www.mysql.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="40" height="40"/> </a> <a href="https://www.python.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a> </p>
+
+<p><img align="left" src="https://github-readme-stats.vercel.app/api/top-langs?username=sayam241020&show_icons=true&locale=en&layout=compact" alt="sayam241020" /></p>
+
+<p>&nbsp;<img align="center" src="https://github-readme-stats.vercel.app/api?username=sayam241020&show_icons=true&locale=en" alt="sayam241020" /></p>
+
+<p><img align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=sayam241020&" alt="sayam241020" /></p>
