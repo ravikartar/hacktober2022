4e0fbfd27362165dbab717b766315cd02ff4272f
ADARSH PRADHAN <77764743+adarsh-pradhan@users.noreply.github.com>
2022-10-20 12:56:36 +0530

    Corrected a typo, and added an easter-egg.

--- Bmi_calculator.py
+++ Bmi_calculator.py
@@ -4,9 +4,14 @@ if height <= 1:
     print("Enter valid height!!!!")
     quit()
 
+# Adding an easter-egg like fun exception for a typo (or really the tallest person!)
+if height > 272:
+    print("Congratulations! You beat the world record for the tallest person!")
+    print("Carry on and find your BMI")
+    
 weight = float(input("Enter your weight in kg: "))
 if weight <= 1:
-    print("Enter valid height!!!!")
+    print("Enter valid weight!!!!")
     quit()
 
 BMI = weight / (height/100)**2
